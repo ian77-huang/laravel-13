ed43bdc4f00f71af9738b14f03242db7cc9bf704
ian77 <ian77.huang@gmail.com>
2026-08-18 14:31:57 +0800

    feat: Update composer scripts to include storage link command and refactor permission functions for consistency

--- config/permissions.php
+++ config/permissions.php
@@ -1,35 +1,39 @@
 <?php
 
-function createAction(string $key, string $value): array
-{
-    $actions = [];
-    $actions[$key] = ['key' => $key, 'value' => $value];
+if (!function_exists('createAction')) {
+    function createAction(string $key, string $value): array
+    {
+        $actions = [];
+        $actions[$key] = ['key' => $key, 'value' => $value];
 
-    return $actions;
+        return $actions;
+    }
 }
 
-function createActions(?array $action = null): array
-{
-    $actions = [
-        ...createAction('Create', 'filament-shield.Create'),
-        ...createAction('Delete', 'filament-shield.Delete'),
-        ...createAction('DeleteAny', 'filament-shield.DeleteAny'),
-        ...createAction('ForceDelete', 'filament-shield.ForceDelete'),
-        ...createAction('ForceDeleteAny', 'filament-shield.ForceDeleteAny'),
-        ...createAction('Replicate', 'filament-shield.Replicate'),
-        ...createAction('Reorder', 'filament-shield.Reorder'),
-        ...createAction('Restore', 'filament-shield.Restore'),
-        ...createAction('RestoreAny', 'filament-shield.RestoreAny'),
-        ...createAction('Update', 'filament-shield.Update'),
-        ...createAction('View', 'filament-shield.View'),
-        ...createAction('ViewAny', 'filament-shield.ViewAny'),
-    ];
+if (!function_exists('createActions')) {
+    function createActions(?array $action = null): array
+    {
+        $actions = [
+            ...createAction('Create', 'filament-shield.Create'),
+            ...createAction('Delete', 'filament-shield.Delete'),
+            ...createAction('DeleteAny', 'filament-shield.DeleteAny'),
+            ...createAction('ForceDelete', 'filament-shield.ForceDelete'),
+            ...createAction('ForceDeleteAny', 'filament-shield.ForceDeleteAny'),
+            ...createAction('Replicate', 'filament-shield.Replicate'),
+            ...createAction('Reorder', 'filament-shield.Reorder'),
+            ...createAction('Restore', 'filament-shield.Restore'),
+            ...createAction('RestoreAny', 'filament-shield.RestoreAny'),
+            ...createAction('Update', 'filament-shield.Update'),
+            ...createAction('View', 'filament-shield.View'),
+            ...createAction('ViewAny', 'filament-shield.ViewAny'),
+        ];
 
-    if ($action && is_array($action)) {
-        $actions = array_merge($actions, $action);
+        if ($action && is_array($action)) {
+            $actions = array_merge($actions, $action);
+        }
+
+        return $actions;
     }
-
-    return $actions;
 }
 
 return [
