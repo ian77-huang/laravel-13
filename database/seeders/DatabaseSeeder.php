32998090661a80b9f0892e44d3d17b677d4eca58
ian77 <ian77.huang@gmail.com>
2026-08-18 19:10:06 +0800

    feat: Add friendships and notifications tables with relationships and indexing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            ShieldSeeder::class,
             UserSeeder::class,
             YoutubeChannelSeeder::class,
             YoutubePlaylistSeeder::class,
